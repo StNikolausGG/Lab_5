9fd2263b69926e9d5f76e87aaf803b2165eafdb0
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-06-02 02:39:04 +0700

    improved list

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct point
 {
@@ -8,45 +9,71 @@ struct point
     struct point* prev;
 };
 
-int AddToHead(struct point ** list)
+struct point* addToHead(struct point **list, int x, int y)
 {
-    if (list && *list)
+    struct point* ptrIx = (struct point*)malloc(sizeof(struct point));
+    if (list && *list && ptrIx)
     {
-
+        ptrIx->x = x;
+        ptrIx->y = y;
+        ptrIx->prev = NULL;
+        ptrIx->next = NULL;
+        if (*list == NULL)
+        {
+            *list = ptrIx;
+        }
+        else
+        {
+            ptrIx->next = *list;
+            *list = ptrIx;
+        }
+        return ptrIx;
     }
+    return NULL;
 }
 
 int main()
 {
-    struct point* head;
-    struct point List1;
-    struct point List2;
-    struct point List3;
-    struct point List4;
-    struct point* ptr = NULL;
-    head->next = &List1;
-    List1.x = 1;
-    List1.y = 1;
-    List1.next = &List2;
-    List1.prev = NULL;
-    List2.x = 2;
-    List2.y = 2;
-    List2.next = &List3;
-    List2.prev = &List1;
-    List3.x = 3;
-    List3.y = 3;
-    List3.next = &List4;
-    List3.prev = &List2;
-    List4.x = 4;
-    List4.y = 4;
-    List4.next = NULL;
-    List4.prev = &List3;
-    head = &List1;
-    ptr = head->next;
+    struct point* head = NULL;
+    struct point* elem1 = (struct point*)malloc(sizeof (struct point));
+    head = elem1;
+    elem1->x = 1;
+    elem1->y = 1;
+    elem1->prev = NULL;
+    elem1->next = NULL;
+    struct point* elem2 = (struct point*)malloc(sizeof (struct point));
+    elem2->x = 2;
+    elem2->y = 2;
+    elem2->prev = NULL;
+    elem2->next = NULL;
+    struct point* elem3 = (struct point*)malloc(sizeof (struct point));
+    elem3->x = 3;
+    elem3->y = 3;
+    elem3->prev = NULL;
+    elem3->next = NULL;
+    struct point* elem4 = (struct point*)malloc(sizeof (struct point));
+    elem4->x = 4;
+    elem4->y = 4;
+    elem4->prev = NULL;
+    elem4->next = NULL;
 
-    while (ptr)
+    elem1->prev = head;
+    elem1->next = elem2;
+    elem2->prev = elem1;
+    elem2->next = elem3;
+    elem3->prev = elem2;
+    elem3->next = elem4;
+    elem4->prev = elem3;
+
+    struct point* newList = addToHead(&head, 5, 5);
+    struct point* ptrIx = NULL;
+    ptrIx = newList;
+
+    while (ptrIx)
     {
-
+        printf("X: %d, ", ptrIx->x);
+        printf("Y: %d\n", ptrIx->y);
+        ptrIx = ptrIx->next;
     }
 
     return 0;
